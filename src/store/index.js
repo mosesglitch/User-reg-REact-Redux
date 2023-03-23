f9c45042365f96306b5ed77cc9180b5dfeeb3973
mosesglitch <mosesgachuru15@gmail.com>
2023-03-23 15:56:18 +0300

    add get request and configure redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { usersReducer } from './slices/usersSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    // albums:albumsApi.reducer
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
-
-export * from './thunks/fetchUsers';
-export * from './thunks/addUser';
-export * from './thunks/removeUser';
+setupListeners(store.dispatch);
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
